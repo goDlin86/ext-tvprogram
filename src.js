8ed358b7c6810eae71f9effc4e4ebda3ed5139fc
goDlin <godlin086@gmail.com>
2019-08-14 07:35:58 +0300

    timeline 20

--- src.js
+++ src.js
@@ -9,7 +9,7 @@ moment.locale('ru')
 
 const channels = ['850', '977', '2060', '1173']
 const hours = [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 0, 1, 2, 3, 4]
-const mins = [0, 10, 20, 30, 40, 50]
+const mins = [0, 20, 40]
 
 const baseUrl = 'https://tv.mail.ru/ajax/channel/?region_id=24&channel_type=&channel_id='
 const urls = [...channels.map(channel => baseUrl + channel + '&date=')]
@@ -32,7 +32,7 @@ class App extends React.Component {
                 urls.map(url => fetch(url + this.state.date.format("YYYY-MM-DD")).then(response => response.json()))
             )
 
-            let programTV = [[],[],[],[]]
+            let programNew = []
             datas.map((tv, i) => {
                 tv.schedule[0].event.current.map(cur => {
                     const time = cur.start
@@ -42,12 +42,36 @@ class App extends React.Component {
                     const hour = parseInt(time.split(":")[0])
                     const min = parseInt(time.split(":")[1])
 
-                    programTV[i].push({ time, title, url, hour, min })
+                    programNew.push({ channel: i, time, title, url, hour, min })
                 })
             })
-            const program = hours.map((hour) => {
-                return { hour, programs: programTV.map((tv) => tv.filter((item) => item.hour == hour)) }
-            })
+            let program = []
+            const hour = moment().hour()
+            const min = moment().minute()
+            hours.map((h) =>
+                mins.map((m) => {
+                    const items = programNew.filter((item) => item.hour == h && Math.floor(item.min/20)*20 == m)
+                    const now = this.state.day == 0 && hour == h && Math.floor(min/20)*20 == m
+                    const style = { top: (min - m)/20 * 100 + '%' }
+                    if (items.length > 0)
+                        program.push([{ time: (h < 10 ? "0" : "") + h + ":" + (m < 10 ? "0" : "") + m, now, style }, ...items])
+                })
+            )
+            // const program = programNew.sort((a, b) => {
+            //     const h1 = a.hour < 5 ? a.hour + 24 : a.hour
+            //     const m1 = a.min
+
+            //     const h2 = b.hour < 5 ? b.hour + 24 : b.hour
+            //     const m2 = b.min
+
+            //     let results = h1 < h2 ? -1 : h1 > h2 ? 1 : 0
+
+            //     if (results === 0) {
+            //         results = m1 < m2 ? -1 : m1 > m2 ? 1 : 0
+            //     }
+               
+            //     return results
+            // })
 
             this.setState({ program })
 
@@ -58,43 +82,25 @@ class App extends React.Component {
     setDate(i) {
         this.setState({ day: i, date: moment().add(i, 'd'), program: [] }, this.fetchData)
     }
-    printTimeline() {
+    printSchedule() {
         const { program, day } = this.state
 
-        const h = day == 0 ? moment().hour() : 5
-        const m = day == 0 ? moment().minute() : 0
-        const lineStyle = { top: m/60 * 100 + '%' }
-        
-        return program.map(({ hour, programs }) => {
-            if (programs.reduce((acc, cur) => acc + cur.length, 0) > 0) {
-                const minPrograms = mins.map((m) => 
-                    programs.map((p) => 
-                        p.filter((item) => Math.floor(item.min/10)*10 == m)
-                    )
-                )
+        return program.map((p, i) => 
+            p.map((item, k) => {
+                const style = { gridColumn: k == 0 ? 1 : item.channel+2, gridRow: i+1, position: 'relative' }
 
                 return (
-                    <div className="hour" key={hour}>
-                        {hour == h && <div className="line" style={lineStyle}>{hour + ":" + (m < 10 ? "0" : "") + m}</div>}
-                        <div className="timeline">{(hour < 10 && "0") + hour + ":00"}</div>
-                        <div className="programs">
-                            {minPrograms.map((p) =>
-                                p.map((tv, j) => 
-                                    <div key={channels[j]}>
-                                        {tv && tv.length > 0 && tv.map((item, i) => 
-                                            <a className="item" href={item.url} target="_blank" key={i}>
-                                                <span className="time">{item.time + ' '}</span>
-                                                <span className={day == 0 && i == 0 ? "title cur" : "title"}>{item.title}</span>
-                                            </a>
-                                        )}
-                                    </div>
-                                )
-                            )}
-                        </div>
+                    <div key={k} style={style}>
+                        {k == 0 && <div className="timeline">{item.time}</div>}
+                        {k == 0 && item.now && <div className="line" style={item.style}></div>}
+                        {k > 0 && <a className="item" href={item.url} target="_blank" key={k}>
+                            <span className="time">{item.time + ' '}</span>
+                            <span className={day == 0 && i == 0 ? "title cur" : "title"}>{item.title}</span>
+                        </a>}
                     </div>
                 )
-            }
-        })     
+            })
+        )
     }
     render() {
         const { day } = this.state
@@ -123,7 +129,7 @@ class App extends React.Component {
                 </header>
 
                 <div className="schedule">
-                    {this.printTimeline()}
+                    {this.printSchedule()}
                 </div>
             </div>
         )
